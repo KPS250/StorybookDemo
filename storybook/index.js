2d3c55ff481b7e4204b67a70f0ca650a392b889b
Kiran Shinde <kiranshinde@Kirans-MacBook-Pro.local>
2023-02-20 19:37:46 +0530

    Fixing Web Ui

--- storybook/index.js
+++ storybook/index.js
@@ -16,10 +16,14 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  port: 7007,
+  host: '192.168.0.103',
+  asyncStorage: null,
+});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
-AppRegistry.registerComponent('%StoryBookDemo%', () => StorybookUIRoot);
+AppRegistry.registerComponent('StoryBookDemo', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
